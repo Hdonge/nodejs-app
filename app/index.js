a69e0a0dfaffa68a37f763657141f5f44ca678a1
Hdonge <harshal.donge@gmail.com>
2018-06-24 13:59:53 +0530

    ping handler added, sample handler removed

--- app/index.js
+++ app/index.js
@@ -108,10 +108,9 @@ var unifiedServer = function (req, res) {
 //define the handlers
 var handlers = {};
 
-//Sample handler
-handlers.sample = function (data, callback) {
-    //callback a http status code and payload object
-    callback(406, { 'name': 'sample handler' });
+//Ping handler
+handlers.ping = function (data, callback) {
+    callback(200);
 }
 
 //Not found handler
@@ -122,5 +121,5 @@ handlers.notFound = function (data, callback) {
 
 //Define a request router
 var router = {
-    'sample': handlers.sample
+    'ping': handlers.ping
 };
