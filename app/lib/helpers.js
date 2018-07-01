606140d7c5be4c8b5a01e9e9c71fcb89e9987389
Hdonge <harshal.donge@gmail.com>
2018-07-01 18:13:29 +0530

    Connecting to an API: added library function to send twilio sms

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -4,7 +4,9 @@
 
 //Dependencies
 const crypto = require('crypto'),
-    config = require('../config');
+    config = require('../config'),
+    queryString = require('querystring'),
+    https = require('https');
 
 
 
@@ -51,6 +53,63 @@ helpers.createRandomString = function (strLength) {
         return str;
     } else {
         return false;
+    }
+};
+
+//Send and SMS via Twilio
+helpers.sendTwilioSms = function (phone, msg, callback) {
+    phone = typeof (phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false;
+    msg = typeof (msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false;
+
+    if (phone && msg) {
+        //Configure the request payload 
+        var payload = {
+            'From': config.twilio.fromPhone,
+            'To': '+91' + phone,
+            'Body': msg
+        };
+
+        //Stringify the payload
+        var stringPayload = queryString.stringify(payload);
+
+        //Configure the request details 
+        var requestDetails = {
+            'protocol': 'https:',
+            'hostname': 'api.twilio.com',
+            'method': 'POST',
+            'path': '/2010-04-01/Accounts/' + config.twilio.accountsid + '/Messages.json',
+            'auth': config.twilio.accountsid + ':' + config.twilio.authToken,
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(stringPayload)
+            }
+        };
+
+        var req = https.request(requestDetails, function (res) {
+            //Grab the status of sent request
+            var status = res.statusCode;
+            //callback successfully if the request went through
+            if (status == 200 || status == 201) {
+                callback(false);
+            } else {
+                callback('Status code return was ' + status);
+            }
+        });
+
+        //Bind to the error event so it doesn't get thrown
+
+        req.on('error', function (e) {
+            callback(e);
+        });
+
+        //Add the payload
+        req.write(stringPayload);
+
+        //End the request
+        req.end();
+
+    } else {
+        callback('Given parameters are missing or invalid');
     }
 };
 
