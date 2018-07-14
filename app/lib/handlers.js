3a46e0fca158ce92dae89d6b82a7db3fd8c74967
Hdonge <harshal.donge@gmail.com>
2018-07-14 18:42:25 +0530

    Create a login session

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -81,6 +81,39 @@ handlers.accountCreate = function (data, callback) {
     }
 };
 
+//Create new session 
+handlers.sessionCreate = function (data, callback) {
+    //Reject any request that is not GET
+    if (data.method == 'get') {
+
+        //Prepare data interpolation 
+        var templateData = {
+            'head.title': 'Login to your account',
+            'head.description': 'Please enter yoour phone number and password to access your account',
+            'body.class': 'sessionCreate'
+        };
+
+        //Read in a template as a string
+        helpers.getTemplate('sessionCreate', templateData, function (err, str) {
+            if (!err && str) {
+                //Add the universal header and footer
+                helpers.addUniversalTemplates(str, templateData, function (err, str) {
+                    if (!err && str) {
+                        //Return the page as HTML
+                        callback(200, str, 'html');
+                    } else {
+                        callback(500, undefined, 'html');
+                    }
+                });
+            } else {
+                callback(500, undefined, 'html');
+            }
+        });
+    } else {
+        callback(405, undefined, 'html');
+    }
+};
+
 /**
  * favicon handler
  */
