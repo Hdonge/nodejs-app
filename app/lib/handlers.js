f44666d7af1b7d533e9bc1a978fc57505170a999
Hdonge <harshal.donge@gmail.com>
2018-07-21 18:37:18 +0530

    Edit check details

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -279,6 +279,37 @@ handlers.checksList = function (data, callback) {
     }
 };
 
+//Edit checks 
+handlers.checksEdit = function (data, callback) {
+    //Reject any request that is not GET
+    if (data.method == 'get') {
+
+        //Prepare data interpolation 
+        var templateData = {
+            'head.title': 'Check Details',
+            'body.class': 'checksEdit'
+        };
+
+        //Read in a template as a string
+        helpers.getTemplate('checksEdit', templateData, function (err, str) {
+            if (!err && str) {
+                //Add the universal header and footer
+                helpers.addUniversalTemplates(str, templateData, function (err, str) {
+                    if (!err && str) {
+                        //Return the page as HTML
+                        callback(200, str, 'html');
+                    } else {
+                        callback(500, undefined, 'html');
+                    }
+                });
+            } else {
+                callback(500, undefined, 'html');
+            }
+        });
+    } else {
+        callback(405, undefined, 'html');
+    }
+};
 
 /**
  * favicon handler
