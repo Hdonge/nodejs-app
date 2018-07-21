d67d88bacdf50630ced043030d846af6f93a6fc3
Hdonge <harshal.donge@gmail.com>
2018-07-21 18:14:42 +0530

    Dashboard- show checklist

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -247,6 +247,38 @@ handlers.checksCreate = function (data, callback) {
     }
 };
 
+//All checks - checklist
+handlers.checksList = function (data, callback) {
+    //Reject any request that is not GET
+    if (data.method == 'get') {
+
+        //Prepare data interpolation 
+        var templateData = {
+            'head.title': 'Dashboard',
+            'body.class': 'checksList'
+        };
+
+        //Read in a template as a string
+        helpers.getTemplate('checksList', templateData, function (err, str) {
+            if (!err && str) {
+                //Add the universal header and footer
+                helpers.addUniversalTemplates(str, templateData, function (err, str) {
+                    if (!err && str) {
+                        //Return the page as HTML
+                        callback(200, str, 'html');
+                    } else {
+                        callback(500, undefined, 'html');
+                    }
+                });
+            } else {
+                callback(500, undefined, 'html');
+            }
+        });
+    } else {
+        callback(405, undefined, 'html');
+    }
+};
+
 
 /**
  * favicon handler
