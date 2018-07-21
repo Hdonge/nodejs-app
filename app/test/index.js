2ec33772d9b40cd5fa3b23c4c97f7580edd6624e
Hdonge <harshal.donge@gmail.com>
2018-07-22 01:05:01 +0530

    Added Test Runner and unit test for helpers.parsedJsonToObject

---
+++ app/test/index.js
@@ -0,0 +1,122 @@
+/**
+ * Test runner
+ */
+
+const helpers = require('./../lib/helpers');
+const assert = require('assert');
+
+//Application logic for test runner
+var _app = {};
+
+//Container for the tests
+_app.tests = {
+    'unit': {}
+};
+
+_app.tests.unit['helpers.parsedJsonToObject should return parsed object when provided stringified object'] = function (done) {
+    let result = helpers.parsedJsonToObject('{"abc":"wxy"}');
+    assert.deepEqual(result, { abc: 'wxy' });
+    done();
+};
+
+_app.tests.unit['helpers.parsedJsonToObject should return empty object when provided corrupted stringified object or bad JSON'] = function (done) {
+    let result = helpers.parsedJsonToObject('{"abc":"wxy');
+    assert.deepEqual(result, {});
+    done();
+};
+
+_app.tests.unit['helpers.parsedJsonToObject should return empty object when provided undefined'] = function (done) {
+    let result = helpers.parsedJsonToObject(undefined);
+    assert.deepEqual(result, {});
+    done();
+};
+
+_app.countTests = function () {
+    var counter = 0;
+    for (let test in _app.tests) {
+        if (_app.tests.hasOwnProperty(test)) {
+            var subTests = _app.tests[test];
+            for (var testName in subTests) {
+                if (subTests.hasOwnProperty(testName)) {
+                    counter++;
+                }
+            }
+        }
+    }
+    return counter;
+};
+
+_app.runTests = function () {
+    var errors = [];
+    var successes = 0;
+    var limit = _app.countTests();
+    var counter = 0;
+    for (let test in _app.tests) {
+        if (_app.tests.hasOwnProperty(test)) {
+            var subTests = _app.tests[test];
+            for (var testName in subTests) {
+                if (subTests.hasOwnProperty(testName)) {
+                    (function () {
+                        var tempTestName = testName;
+                        var testValue = subTests[testName];
+
+                        //Call the test
+                        try {
+                            testValue(function () {
+                                // If it calls back without throwing, then it succeeded, so log it in green
+                                console.log('\x1b[32m%s\x1b[0m', tempTestName);
+                                counter++;
+                                successes++;
+                                if (counter == limit) {
+                                    _app.produceTestReport(limit, successes, errors);
+                                }
+                            });
+                        } catch (e) {
+                            // If it throws, then it failed, so capture the error thrown and log it in red
+                            errors.push({
+                                'name': testName,
+                                'error': e
+                            });
+                            console.log('\x1b[31m%s\x1b[0m', tempTestName);
+                            counter++;
+                            if (counter == limit) {
+                                _app.produceTestReport(limit, successes, errors);
+                            }
+                        }
+                    })();
+                }
+            }
+        }
+    }
+};
+
+// Product a test outcome report
+_app.produceTestReport = function (limit, successes, errors) {
+    console.log("");
+    console.log("--------BEGIN TEST REPORT--------");
+    console.log("");
+    console.log("Total Tests: ", limit);
+    console.log("Pass: ", successes);
+    console.log("Fail: ", errors.length);
+    console.log("");
+
+    // If there are errors, print them in detail
+    if (errors.length > 0) {
+        console.log("--------BEGIN ERROR DETAILS--------");
+        console.log("");
+        errors.forEach(function (testError) {
+            console.log('\x1b[31m%s\x1b[0m', testError.name);
+            console.log(testError.error);
+            console.log("");
+        });
+        console.log("");
+        console.log("--------END ERROR DETAILS--------");
+    }
+
+
+    console.log("");
+    console.log("--------END TEST REPORT--------");
+
+};
+
+_app.runTests();
